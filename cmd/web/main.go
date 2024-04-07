b659e5773476cb1b2201092e0e2a209fbd8ef8ed
vladgershun <dev.vladgershun@gmail.com>
2024-04-07 17:29:04 -0500

    Lets Go - 3.5 completed

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -19,19 +19,9 @@ func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
 	app := &application{logger: logger}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("starting server", slog.String("port", *port))
 
-	err := http.ListenAndServe(*port, mux)
+	err := http.ListenAndServe(*port, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
