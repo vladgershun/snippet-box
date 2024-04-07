ef00e3b98f1b51a0b11f2cb31fa7148358e0424f
vladgershun <dev.vladgershun@gmail.com>
2024-04-07 11:17:14 -0500

    Go Further 2.6 complete

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("Running server on port :2020")
-	err := http.ListenAndServe(":2020", mux)
+	log.Print("Running server on port :4000")
+
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to my server!"))
+	w.Header().Add("Server", "Go")
+	w.Write([]byte("Welcome to Snippetbox!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
 }
