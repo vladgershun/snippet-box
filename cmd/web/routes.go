1ae7e735c487d30658faaac060f1002ba9dca687
vladgershun <dev.vladgershun@gmail.com>
2024-04-09 20:07:34 -0500

    Lets Go - 6.5 completed

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
@@ -15,5 +17,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+	return standard.Then(mux)
 }
