401040c0d20d9e1bafc4fc1c6e5a83cb40a17583
vladgershun <dev.vladgershun@gmail.com>
2024-04-10 20:32:57 -0500

    Lets Go - 10.7 completed

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -47,6 +49,7 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 )
