d9b37d16659a315d8a336a7ea66973f25e126905
vladgershun <dev.vladgershun@gmail.com>
2024-04-11 17:45:15 -0500

    Lets Go - 11.2 completed

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -61,12 +62,35 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 }
 
 func noSurf(next http.Handler) http.Handler {
-    csrfHandler := nosurf.New(next)
-    csrfHandler.SetBaseCookie(http.Cookie{
-        HttpOnly: true,
-        Path:     "/",
-        Secure:   true,
-    })
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
 
-    return csrfHandler
+	return csrfHandler
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		exists, err := app.users.Exists(id)
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
