5f06c6da5e09432d49abfc47f6342d899fffb0e3
vladgershun <dev.vladgershun@gmail.com>
2024-04-10 20:03:19 -0500

    Lets Go - 10.6 completed

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -44,7 +44,12 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
