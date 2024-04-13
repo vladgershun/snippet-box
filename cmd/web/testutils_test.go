50e73f34ecbc821a08157d27045b65eeaa342688
vladgershun <dev.vladgershun@gmail.com>
2024-04-13 14:20:46 -0500

    Lets Go - 13.6 completed

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bytes"
+	"html"
 	"io"
 	"log/slog"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 
@@ -14,25 +17,31 @@ import (
 	"github.com/vladgershun/snippet-box/internal/models/mocks"
 )
 
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(string(matches[1]))
+}
+
 func newTestApplication(t *testing.T) *application {
-	// Create an instance of the template cache.
 	templateCache, err := newTemplateCache()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// And a session manager instance. Note that we use the same settings as
-	// production, except that we *don't* set a Store for the session manager.
-	// If no store is set, the SCS package will default to using a transient
-	// in-memory store, which is ideal for testing purposes.
 	sessionManager := scs.New()
 	sessionManager.Lifetime = 12 * time.Hour
 	sessionManager.Cookie.Secure = true
 
 	return &application{
 		logger:         slog.New(slog.NewTextHandler(io.Discard, nil)),
-		snippets:       &mocks.SnippetModel{}, // Use the mock.
-		users:          &mocks.UserModel{},    // Use the mock.
+		snippets:       &mocks.SnippetModel{},
+		users:          &mocks.UserModel{},
 		templateCache:  templateCache,
 		sessionManager: sessionManager,
 	}
@@ -61,6 +70,22 @@ func newTestServer(t *testing.T, h http.Handler) *testServer {
 
 func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
 	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
 	if err != nil {
 		t.Fatal(err)
 	}
